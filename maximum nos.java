25b246a89ab2d9e2c3f7eeba4a285769f30240d4
divyaraut17 <divyaraut287@gmail.com>
2025-06-24 12:41:21 +0530

    Create maximum nos.java

---
+++ maximum nos.java
@@ -0,0 +1,25 @@
+package com.dkte;
+
+public class Max {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int n1= 10;
+       int n2 = 70;
+       int n3 = 71;
+       
+       if(n1 > n2 && n1 >n3 )
+       {
+    	   System.out.println("The maximum no is :"+ n1);
+       }
+       else if(n2 > n3 && n2> n3)
+       {
+    	   System.out.println("The maximum no is:"+n2);
+       }
+       else {
+    	   System.out.println("The maximum no is:"+n3);
+       }
+       
+	}
+
+}
