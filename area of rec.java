2672d3780d3fa5abfc4ae755d4e4f0d2046f6beb
divyaraut17 <divyaraut287@gmail.com>
2025-06-24 12:46:30 +0530

    Create area of rec.java

---
+++ area of rec.java
@@ -0,0 +1,21 @@
+package com.dkte;
+
+public class area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int length = 4;
+			int width = 5;
+			int area ;
+			
+			area =length * width;
+			
+			System.out.println("length is:"+ length);
+			System.out.println("width is :"+ width);
+			System.out.println("Area of rectangle is:" + area);
+		
+
+	}
+
+}
