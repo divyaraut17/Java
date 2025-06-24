c292c907e796db5007bb3ac6c59e93c514b3a89d
divyaraut17 <divyaraut287@gmail.com>
2025-06-24 12:42:48 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+package com.dkte;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int j,i,n=5;
+       for(i=n;i>=1;i--)
+       {
+    	   for(j=1;j<=i;j++)
+    	   {
+    		   System.out.print("*");
+    	   }
+    	   System.out.println();
+       }
+	}
+
+}
