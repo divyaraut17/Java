6f6afbcc74c4ab3cf33c6b9fc1ae6f709be2857d
divyaraut17 <divyaraut287@gmail.com>
2025-07-02 12:00:26 +0530

    Add files via upload

---
+++ day2/src/com/dkte/pattern.java
@@ -0,0 +1,18 @@
+package com.dkte;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int j,i,n=5;
+       for(i=5;i>=1;i--)
+       {
+    	   for(j=1;j<=i;j++)
+    	   {
+    		   System.out.print("*");
+    	   }
+    	   System.out.println();
+       }
+	}
+
+}
