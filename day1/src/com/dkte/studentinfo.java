d9c78a977ebc5b50736b24b9ded9b88421b70a7c
divyaraut17 <divyaraut287@gmail.com>
2025-07-02 11:59:57 +0530

    Add files via upload

---
+++ day1/src/com/dkte/studentinfo.java
@@ -0,0 +1,21 @@
+package com.dkte;
+
+public class studentinfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String studentName = "John Doe";
+		int studentID = 15;
+		int studentAge = 23;
+		float studentFee = 75.25f;
+		char studentGrade = 'B';
+
+		
+		System.out.println("Student name: " + studentName);
+		System.out.println("Student id: " + studentID);
+		System.out.println("Student age: " + studentAge);
+		System.out.println("Student fee: " + studentFee);
+		System.out.println("Student grade: " + studentGrade);
+	}
+
+}
