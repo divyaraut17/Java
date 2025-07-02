5805528929ea8aa7f370ad76f69a70e98c6b7840
divyaraut17 <divyaraut287@gmail.com>
2025-07-02 12:13:40 +0530

    Add files via upload

---
+++ java/Program.java
@@ -0,0 +1,10 @@
+package main;
+import math.Arithmetic;
+class Program{
+     public static void main(String[] args){
+      Arithmetic obj = new Arithmetic();
+      obj.add(10,20);
+      obj.sub(20,10);
+}
+}
+    
